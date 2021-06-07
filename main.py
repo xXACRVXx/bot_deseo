48821046af0191e70c7302a7b84449605fab698c
xXACRVXx <70526454+xXACRVXx@users.noreply.github.com>
2021-06-07 08:50:50 -0400

    update main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import os
-import telegram
 import sys
-from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 TOKEN = os.getenv("TOKEN")
@@ -13,40 +11,28 @@ print('DE @xXACRVXx (Abrahán) en python 3.8')
 def start(update, context):
       update.message.reply_text("""Bot de deseos de @hentai_s3 VERSION: 0.0.1 (ALPHA)""")
 
-
 def mensajes_entrantes(update, context):
      Texto= update.message.text
      Grupo= update.message.chat.title
      Usuario= update.effective_user['first_name']
      
-     
-     
      if str(Texto).__contains__('#deseo'):
-      context.bot.send_message(chat_id='@grupo_para_probar_bots',text='Grupo:' + str(Grupo).replace('None', 'privado' ) + '\n' +'Usuario: ' + str(Usuario).replace('None','Anónimo') + '\n\n ' + str(Texto) +  '\n\n')
-     
-     
-     
-     
-     
+      context.bot.send_message(chat_id='@deseos_solo_hentai_desu',text='Grupo:' + str(Grupo).replace('None', 'privado' ) + '\n' +'Usuario: ' + str(Usuario).replace('None','Anónimo') + '\n\n ' + str(Texto) +  '\n\n')
+    
 if __name__ == "__main__":
   
     elbot = telegram.Bot(token=TOKEN)
     
 actualizador = Updater(elbot.token, use_context=True)
        
-  
 despachador = actualizador.dispatcher
-
 
 despachador.add_handler(CommandHandler('start', start))
 
-
 despachador.add_handler(MessageHandler(filters=Filters.text, callback= mensajes_entrantes))
-   
    
 print('\nIniciando\n')
   
 actualizador.start_polling()
   
 actualizador.idle   
-   
