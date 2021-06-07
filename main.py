292d20a0d4274ff02dfe375bb4ff0610063bd443
xXACRVXx <70526454+xXACRVXx@users.noreply.github.com>
2021-06-07 05:29:36 -0400

    update main.py

--- main.py
+++ main.py
@@ -1,1 +1,44 @@
+import os
+import telegram
+import sys
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+TOKEN = os.getenv("TOKEN")
+
+
+
+print('\n\n')
+
+
+
+print('                #################')
+print('              ###   ###############')
+print('              ###   ###############')
+print('              #####################')
+print('                        ###########')
+print('       ############################ ,,,,,,')
+print('     ############################## ,,,,,,,,.')
+print('    ############################### ,,,,,,,,,.')
+print('   ################################ ,,,,,,,,,,')
+print('   ###############################  ,,,,,,,,,,')
+print('   ##############                 ,,,,,,,,,,,,')
+print('   ###########  .,,,,,,,,,,,,,,,,,,,,,,,,,,,,,')
+print('   ##########  ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,')
+print('   ########## ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,.')
+print('    ######### ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,.')
+print('      ####### ,,,,,,,,,,,,,,,,,,,,,,,,,,,,')
+print('              ,,,,,,,,,,,')
+print('              ,,,,,,,,,,,,,,,,,,,,,')
+print('              ,,,,,,,,,,,,,,,   ,,,')
+print('              .,,,,,,,,,,,,,,   ,,,')
+print('                ,,,,,,,,,,,,,,,,,,')
+
+#print('\nEl que me robe el logo lo demando, me llevo 40m -_-')
+
+
+print('\n\n\n')
+print('PROYECTO SHIROMI VERSION: 0.0.1 (ALPHA)')
+print('DE @xXACRVXx (Abrahán) en python 3.8')
+
 
