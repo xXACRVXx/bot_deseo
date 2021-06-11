ca39bc561618ebd3fc3dd0e11d37cc1afd45f644
xXACRVXx <70526454+xXACRVXx@users.noreply.github.com>
2021-06-11 05:34:29 -0400

    update main.py

--- main.py
+++ main.py
@@ -18,8 +18,11 @@ def mensajes_entrantes(update, context):
      Grupo= update.message.chat.title
      Usuario= update.effective_user['first_name']
      
-     if str(Texto).__contains__('#deseo'):
+     if str(Texto).startswith('#deseo'):
       context.bot.send_message(chat_id='-1001407312660',text='Grupo:' + str(Grupo).replace('None', 'privado' ) + '\n' +'Usuario: ' + str(Usuario).replace('None','Anónimo') + '\n\n ' + str(Texto) +  '\n\n')
+      
+def start(update, context):
+      update.message.reply_text( 'Historial de cambios ver: 0.1.23\nOptimizacion de de lineas de código, paso de 87 a 45 lineas\ncorrección de detección del #deseo (ahora solo funcionan si es la primera palabra)\n corrección de errores en el log\n añadida la base para futura versión con una lista de los últimos deseos enviados al bot\ncorrecion/actualizacion de la versión en el /start' )
     
 if __name__ == "__main__":
   
@@ -32,6 +35,8 @@ despachador = actualizador.dispatcher
 despachador.add_handler(CommandHandler('start', start))
 
 despachador.add_handler(MessageHandler(filters=Filters.text, callback= mensajes_entrantes))
+
+despachador.add_handler(CommandHandler('updates', updates))
    
 print('\nIniciando\n')
   
