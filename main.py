c4ee3a803bcf3a027d9fa3df1f1a16649a85f895
xXACRVXx <70526454+xXACRVXx@users.noreply.github.com>
2021-06-13 08:23:47 -0400

    update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def start(update, context):
      
 
 def updates(update, context):
-      update.message.reply_text( """Historial de cambios ver: 0.1.27(beta)\n\nSe agrego el @ del usuario en el deseo para ayudar a encontrarlo en el grupo (en caso de el usuario no contara con @ se dará el id aunque aún mo lo e probado)\n\nver: 0.1.26(beta)\n\nCorrección de errores y mejoras de seguridad, como el  toDus :v\n\nversion: 0.25(Alpha)\n\nEsta versión fue cancelada en estado de Alpha por problemas con el reenvío.\nF por ella""" )
+      update.message.reply_text( """Historial de cambios ver: 0.1.27(beta)\n\nSe agrego el @ del usuario en el deseo para ayudar a encontrarlo en el grupo (en caso de el usuario no contara con @ se dará el id aunque aún mo lo e probado)\n\n-ver: 0.1.26(beta)\n\nCorrección de errores y mejoras de seguridad, como el  toDus :v\n\n-ver: 0.1.25(Alpha)\n\nEsta versión fue cancelada en estado de Alpha por problemas con el reenvío.\nF por ella""" )
 
 def mensajes_entrantes(update, context):
      Texto= update.message.text
@@ -23,6 +23,8 @@ def mensajes_entrantes(update, context):
      
      if str(Texto).startswith('#deseo'):
       context.bot.send_message(chat_id='-1001407312660',text='Grupo:' + str(Grupo).replace('None', 'privado' ) + '\n' +'Usuario: ' + str(Usuario).replace('None','Anónimo')+' @'+str(Usuario2) + '\n\n ' + str(Texto) +  '\n\n')
+      
+      update.message.reply_text("Tu mensaje fue enviado")
       
 
     
