a6bc9f838d7b4e9216895c87c8e4e72d7b4dc238
xXACRVXx <70526454+xXACRVXx@users.noreply.github.com>
2021-07-17 21:21:08 -0400

    update main.py

--- main.py
+++ main.py
@@ -1,21 +1,59 @@
+# este bot forma parte de el repositorio de GitHub https://github.com/xXACRVXx/bot_deseo bajo licencia MIT
+
 import os
 import sys
 import telegram
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-#TOKEN contiene el token de telegram necesario para que el bot funcione se extrae en este caso de la variable de entorno configurada en Heroku
-TOKEN = os.getenv("TOKEN")
+# TOKEN contiene el token de telegram necesario para que el bot funcione se extrae en este caso de la variable de entorno configurada en Heroku
+#TOKEN = os.getenv("TOKEN")
+
+TOKEN ="1858921469:AAEw1TIz5JkiRz-KS6_QUUak_97rE0idc0M"
 
 
-#la lista info = [] controla el reenvío de arcivos del bot se guarda str("1") si el bot es desactivado por los admiradores o se deja vacía para que funcione
-info = []
+# el # para reenviar los archivos
+Hastag1 = '#hentai'
+
+
+# el # para enviar sugerencias al canal de los administradores
+Hastag2 = '#deseo'
+
+
+# aqui se pone el grupo o canal al que desea reenviar los mensajes que contengan #hentai
+Canal_hastag1= '@solo_hentai_s3'
+
+
+
+
+# aqui se pone el grupo o canal al que desea reenviar los mensajes que contengan #deseo
+Canal_hastag2 ='-1001407312660'
+
+
+
+
+# grupo donde se encuentran los administradores de el bot
+Admins_Grupo = '-1001255367733'
+
+
+# aquí terminan los parámetros editables de el bot
+##############################################################################
+
+
+
+# modo = [] controla el reenvío de arcivos del bot se guarda str("1") si el bot es desactivado por los admiradores o se deja vacía para que funcione
+modo = []
+
+# lista de usuarios ignorados por el bot
+ignore = []
+
+
 
 print('By Python 3.8')
 
-#def admins(Contextbot, Usuario_id ): controla los administradores devolviendo EsAdmin = True en caso de que el usuario sea administrador de el grupo definido en ChatId
+# def admins(Contextbot, Usuario_id ): controla los administradores devolviendo EsAdmin = True en caso de que el usuario sea administrador de el grupo definido en ChatId
 def admins(Contextbot, Usuario_id ):
   
-  ChatId = '-1001255367733'
+  ChatId = Admins_Grupo
   
   GrupoAdmins = Contextbot.get_chat_administrators(ChatId)
   
@@ -26,7 +64,7 @@ def admins(Contextbot, Usuario_id ):
   return EsAdmin
 
 
-#el comando /start cumple 2 funciones, si un usuario no es administrador responde un mensaje para indicar que está funcionando correctamente pero si el usuario es un administrador aciva el reenvío de archivos en caso de estar desactivado
+# el comando /start cumple 2 funciones, si un usuario no es administrador responde un mensaje para indicar que está funcionando correctamente pero si el usuario es un administrador aciva el reenvío de archivos en caso de estar desactivado
 def start(update, context):
           
    Usuario2 =update.effective_user['username']
@@ -43,17 +81,17 @@ def start(update, context):
       
       update.message.reply_text(f"@{Usuario2}-Sama El reenvío de multimedia a sido activado ")
       
-      if info.__contains__('1'):
-         info.remove('1')
-         return info
-         print(info)
+      if modo.__contains__('1'):
+         modo.remove('1')
+         return modo
+         print(modo)
       
          
    else:   
      
       update.message.reply_text("""Bot hentai_s3 funcionando""")
 
-#el comando /stop cumplela funcion contraria al /start desactivando el reenvio de archivos
+# el comando /stop cumplela funcion contraria al /start desactivando el reenvio de archivos
 def stop(update, context):
       
    Usuario2 =update.effective_user['username']
@@ -67,15 +105,15 @@ def stop(update, context):
 
    if admins(Contextbot, Usuario_id) == True :  
       update.message.reply_text(f"@{Usuario2}-Sama El reenvío de multimedia a sido desactivado ")
-      info.append('1')
-      print(info)
-      return info
+      modo.append('1')
+      print(modo)
+      return modo
 
-#el comando /updates muestra el Historial de cambios de las actualizaciones del bot
+# el comando /updates muestra el Historial de cambios de las actualizaciones del bot
 def updates(update, context):
-      update.message.reply_text( """Historial de cambios ver: 1.0.0\n\nInformacion clasificada hasta que me entren ganas de escribir """ )
+      update.message.reply_text( """Historial de cambios ver: 1.5.7\n\ninformacion clasificada hasta que me entren ganas de escribir """ )
 
-#en def mensajes_entrantes(update, context): es donde se maneja la mayoría de mensajes que entran y salen hacia Telegram convirtiendolo en una de las partes fundamentales del bot
+# en def mensajes_entrantes(update, context): es donde se maneja la mayoría de mensajes que entran y salen hacia Telegram convirtiendolo en una de las partes fundamentales del bot
 def mensajes_entrantes(update, context):
      Texto= update.message.text
      Grupo= update.message.chat.title
@@ -84,7 +122,7 @@ def mensajes_entrantes(update, context):
      
      Id_grupo = update.message.chat.id
      
-     #se utiliza en los archivos que contienen texto
+     # Subtitulos se utiliza en los archivos que contienen texto
      Subtitulos = update.message.caption
      
      Usuario_id = update.effective_user['id']
@@ -92,77 +130,77 @@ def mensajes_entrantes(update, context):
      Contextbot = context.bot
    
    
-     #esto es opcional solo esta aquí como alternativa a /start y /stop en caso de existir muchos bots en un grupo y asi no tengan que responder todos al conando /start
+     # esto es opcional solo esta aquí como alternativa a /start y /stop en caso de existir muchos bots en un grupo y asi no tengan que responder todos al conando /start
      if admins(Contextbot, Usuario_id) == True :  
        if str(Texto).startswith('-off'):
           update.message.reply_text(f"@{Usuario2}-Sama El reenvío de multimedia a sido desactivado ")
-          info.append('1')
-          print(info)
-          return info
+          modo.append('1')
+          print(modo)
+          return modo
           
           update.message.reply_text(f"@{Usuario2}-Sama El reenvío de multimedia a sido activado ")
       
        if str(Texto).startswith('-on'):
-         if info.__contains__('1'):
-          info.remove('1')
-          return info
-          print(info)
+         if modo.__contains__('1'):
+          modo.remove('1')
+          return modo
+          print(modo)
        
-       if str(Texto).startswith('-info'):
+       if str(Texto).startswith('-modo'):
          
-          update.message.reply_text(str(info))
+          update.message.reply_text(str(modo))
         
      
-     #aqui es donde se permite o no el reenvío de archivos si la lista info=[] no contiene '1'
-     if not info.__contains__('1') :
+     # aqui es donde se permite o no el reenvío de archivos si la lista modo=[] no contiene '1'
+     if not modo.__contains__('1') :
        
-         #si el usuario que utiliza el #hentai no es Telegram se reenviar los archivos o el mensaje esto se utiliza para evitar que el bot caiga en bucle
+         # si el usuario que utiliza el #hentai no es Telegram se reenviar los archivos o el mensaje esto se utiliza para evitar que el bot caiga en bucle
          if not str(Usuario) == 'Telegram':
            
-           if str(Subtitulos).__contains__('#hentai'):
+           if str(Subtitulos).__contains__(Hastag1):
             Id_mensage = update.message.message_id
-            context.bot.forward_message(chat_id='@solo_hentai_s3',from_chat_id = Id_grupo , message_id= Id_mensage )
+            context.bot.forward_message(chat_id=Canal_hastag1 ,from_chat_id = Id_grupo , message_id= Id_mensage )
            
-           if str(Texto).startswith('#hentai'):
+           if str(Texto).startswith(Hastag1):
             Id_mensage_re = update.message.reply_to_message.message_id
             
-            context.bot.forward_message(chat_id='@solo_hentai_s3',from_chat_id = Id_grupo , message_id= Id_mensage_re )
+            context.bot.forward_message(chat_id=Canal_hastag1 ,from_chat_id = Id_grupo , message_id= Id_mensage_re )
             update.message.reply_text("Tu mensaje fue enviado")
      
-     #en caso de estar desactivado el reenvío de el bot y se envie un mensaje que contenga #hentai se enviara un mensaje como feedback al usuario para que sepa la razon por la cual no funcionó
+     # en caso de estar desactivado el reenvío de el bot y se envie un mensaje que contenga #hentai se enviara un mensaje como feedback al usuario para que sepa la razon por la cual no funcionó
      else:
        
         if not str(Usuario) == 'Telegram':
            
-           if str(Subtitulos).__contains__('#hentai'):
+           if str(Subtitulos).__contains__(Hastag1):
              
             update.message.reply_text("El reenvío fue desactivado")
      
-           if str(Texto).startswith('#hentai'):
+           if str(Texto).startswith(Hastag1):
      
             update.message.reply_text("El reenvío fue desactivado")
      
       
-     #si se envia el texto #deseo un grupo donde se encuentre el bot este reenviará el mensaje a el grupo establecido en chat_id='' y devolvera un mensaje de feedback  
-     if str(Texto).startswith('#deseo'):
-      context.bot.send_message(chat_id='-1001407312660',text=f"Grupo:{str(Grupo).replace('None', 'privado' )}\nUsuario: {str(Usuario).replace('None','Anónimo')} @{str(Usuario2)}\n\nt.me/{update.message.chat.username}/{update.message.message_id}\n\n{str(Texto)}")
+     # si se envia el texto #deseo un grupo donde se encuentre el bot este reenviará el mensaje a el grupo establecido en chat_id='' y devolvera un mensaje de feedback  
+     if str(Texto).startswith(Hastag2):
+      context.bot.send_message(chat_id=Canal_hastag2,text=f"Grupo:{str(Grupo).replace('None', 'privado' )}\nUsuario: {str(Usuario).replace('None','Anónimo')} @{str(Usuario2)}\n\nt.me/{update.message.chat.username}/{update.message.message_id}\n\n{str(Texto)}")
       
       update.message.reply_text("Tu deseo fue enviado")
       
 
-#aqui comienza la ejecución del bot   
+# aqui comienza la ejecución del bot   
 if __name__ == "__main__":
   
-    #el objeto Hentaibot contiene el token de telegram obtenido de la variable TOKEN y otros datos 
+    # el objeto Hentaibot contiene el token de telegram obtenido de la variable TOKEN y otros datos 
     Hentaibot = telegram.Bot(token=TOKEN)
  
- #actualizador contiene el objeto Hentaibot y extrae el token de telegram mas use_context=True necesario para el funcionamiento de el bot
+ # actualizador contiene el objeto Hentaibot y extrae el token de telegram mas use_context=True necesario para el funcionamiento de el bot
 actualizador = Updater(Hentaibot.token, use_context=True)
  
- #despachador normalmente nombrado dp extrae el dispatcher de el objeto actualizador      
+ # despachador normalmente nombrado dp extrae el dispatcher de el objeto actualizador      
 despachador = actualizador.dispatcher
 
-#aqui se definen los comandos a los que reacciona el bot
+# aqui se definen los comandos a los que reacciona el bot
 despachador.add_handler(CommandHandler('start', start))
 
 despachador.add_handler(CommandHandler('stop', stop))
@@ -177,4 +215,4 @@ print('\nIniciando\n')
   
 actualizador.start_polling()
   
-actualizador.idle  
+actualizador.idle   
